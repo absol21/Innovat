522a35a8fc00323697eeb414186df37f539381d7
ermek <2006ermek2006@gmail.com>
2023-08-08 16:54:16 +0600

    .

--- apps/exchange/admin.py
+++ apps/exchange/admin.py
@@ -4,3 +4,7 @@ from . import models
 @admin.register(models.Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ['title', 'owner', 'image', 'author']
+
+admin.site.register(models.Author)
+admin.site.register(models.Genre)
+admin.site.register(models.Language)
