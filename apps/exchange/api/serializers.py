9f3a113da9da3c5d9f39864cc7a9a35fe77323bf
Tazabek <nurdinovtazabek@gmail.com>
2023-08-18 02:40:11 +0600

    adding status and condition fields

--- apps/exchange/api/serializers.py
+++ apps/exchange/api/serializers.py
@@ -54,7 +54,7 @@ class AddBookSerializer(serializers.ModelSerializer):
     language = serializers.CharField()
     class Meta:
         model = Book
-        fields = ['title', 'status', 'image', 'author', 'language', 'genre', 'description']
+        fields = ['title', 'status', 'condition', 'image', 'author', 'language', 'genre', 'description']
 
     def create(self, validated_data):
         user = self.context['request'].user
@@ -81,7 +81,8 @@ class AddBookSerializer(serializers.ModelSerializer):
             image = validated_data['image'],
             language = language,
             author = author,
-            description = validated_data['description']
+            description = validated_data['description'],
+            condition = validated_data['condition']
         )
         book.save()
         for i in validated_data['genre']:
--- main/settings.py
+++ main/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'USER': config('DB_USER'),
         'HOST': 'localhost',
         'PORT': 5432
-    }
+        }
 }
 
 # Password validation
